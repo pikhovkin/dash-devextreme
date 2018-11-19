46f3c9ecbde9ab50669296ed9746a67f2b953a02
Sergey Pikhovkin <pikhovkin@gmail.com>
2018-11-19 22:48:17 +0300

    Update usage.py

--- usage.py
+++ usage.py
@@ -1,11 +1,53 @@
-import dash_devextreme as ddx
 import dash
 from dash.dependencies import Input, Output
 import dash_html_components as html
 
+import dash_devextreme as ddx
 
-app = dash.Dash(__name__)
 
+data = [
+    dict(id=1, parent_id=None, dept='Sales', director='Anna'),
+    dict(id=2, parent_id=1, dept='Sales 1', director='Bella'),
+    dict(id=3, parent_id=1, dept='Sales 2', director='Ganna'),
+    dict(id=4, parent_id=None, dept='Developers', director='Donna'),
+    dict(id=5, parent_id=4, dept='Developers N', director='Elly'),
+]
+columns = [
+    dict(caption='Department', dataField='dept', cellTemplate='dept'),
+    dict(caption='Director', dataField='director'),
+]
+
+
+_default_index = '''<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+        <link rel="stylesheet" media="all" href="https://cdn3.devexpress.com/jslib/18.2.3/css/dx.common.css">
+        <link rel="stylesheet" media="all" href="https://cdn3.devexpress.com/jslib/18.2.3/css/dx.light.css">
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            
+            <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+            
+            {%scripts%}
+            
+            <script type="text/jsx" data-presets="react">
+                function dept(props){
+                    return <b>{props.value}</b>
+                }
+            </script>
+        </footer>
+    </body>
+</html>'''
+
+
+app = dash.Dash(__name__, index_string=_default_index)
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
 
@@ -15,9 +57,10 @@ app.layout = html.Div([
         value='my-value',
         valueChangeEvent='input'
     ),
-    html.Div(id='output')
+    html.Div(id='output'),
+    ddx.TreeList(dataSource=data, columns=columns, keyExpr='id', parentIdExpr='parent_id'),
+    ddx.DataGrid(dataSource=data, columns=columns),
 ])
-
 
 @app.callback(Output('output', 'children'), [Input('input', 'value')])
 def display_output(value):
