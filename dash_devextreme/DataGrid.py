3e9460f4326969d85ccac7ec9bde66d2f125dcc2
Sergey Pikhovkin <pikhovkin@gmail.com>
2018-11-25 20:09:29 +0300

    Update DataGrid component

--- dash_devextreme/DataGrid.py
+++ dash_devextreme/DataGrid.py
@@ -9,21 +9,87 @@ class DataGrid(Component):
 
 Keyword arguments:
 - id (string; optional)
-- dataSource (dict; optional)
-- columns (list; optional)
+- accessKey (string; optional)
+- activeStateEnabled (boolean; optional)
+- allowColumnReordering (boolean; optional)
+- allowColumnResizing (boolean; optional)
+- cacheEnabled (boolean; optional)
+- cellHintEnabled (boolean; optional)
 - columnAutoWidth (boolean; optional)
+- columnChooser (dict; optional)
+- columnFixing (dict; optional)
+- columnHidingEnabled (boolean; optional)
+- columnMinWidth (number; optional)
+- columnResizingMode (a value equal to: 'nextColumn', 'widget'; optional)
+- columns (list; optional)
+- columnWidth (number; optional)
+- dataSource (dict | list; optional)
+- dateSerializationFormat (string; optional)
+- disabled (boolean; optional)
+- editing (dict; optional)
+- elementAttr (dict; optional)
+- errorRowEnabled (boolean; optional)
+- export (dict; optional)
+- filterBuilder (dict; optional)
+- filterBuilderPopup (dict; optional)
+- filterPanel (dict; optional)
+- filterRow (dict; optional)
+- filterSyncEnabled (boolean; optional)
+- filterValue (dict; optional)
+- focusedColumnIndex (number; optional)
+- focusedRowEnabled (boolean; optional)
+- focusedRowIndex (number; optional)
+- focusedRowKey (boolean | number | string | dict | list; optional)
+- focusStateEnabled (boolean; optional)
 - grouping (dict; optional)
+- groupPanel (dict; optional)
+- headerFilter (dict; optional)
+- height (number | string; optional)
+- highlightChanges (boolean; optional)
+- hint (string; optional)
+- hoverStateEnabled (boolean; optional)
+- keyExpr (string | list; optional)
+- loadPanel (dict; optional)
+- masterDetail (dict; optional)
+- noDataText (string; optional)
+- onCellClick (string; optional)
+- onRowClick (string; optional)
+- pager (dict; optional)
+- paging (dict; optional)
+- remoteOperations (boolean | dict; optional)
+- renderAsync (boolean; optional)
+- repaintChangesOnly (boolean; optional)
+- rowAlternationEnabled (boolean; optional)
 - rowTemplate (string; optional)
+- rtlEnabled (boolean; optional)
+- scrolling (dict; optional)
+- searchPanel (dict; optional)
+- selectedRowKeys (list; optional)
+- selection (dict; optional)
+- selectionFilter (list; optional)
+- showBorders (boolean; optional)
+- showColumnHeaders (boolean; optional)
+- showColumnLines (boolean; optional)
+- showRowLines (boolean; optional)
+- sortByGroupSummaryInfo (list; optional)
+- sorting (dict; optional)
+- stateStoring (dict; optional)
+- summary (dict; optional)
+- tabIndex (number; optional)
+- twoWayBindingEnabled (boolean; optional)
+- visible (boolean; optional)
+- width (number | string; optional)
+- wordWrapEnabled (boolean; optional)
 
 Available events: """
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, dataSource=Component.UNDEFINED, columns=Component.UNDEFINED, columnAutoWidth=Component.UNDEFINED, grouping=Component.UNDEFINED, rowTemplate=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'dataSource', 'columns', 'columnAutoWidth', 'grouping', 'rowTemplate']
+    def __init__(self, id=Component.UNDEFINED, accessKey=Component.UNDEFINED, activeStateEnabled=Component.UNDEFINED, allowColumnReordering=Component.UNDEFINED, allowColumnResizing=Component.UNDEFINED, cacheEnabled=Component.UNDEFINED, cellHintEnabled=Component.UNDEFINED, columnAutoWidth=Component.UNDEFINED, columnChooser=Component.UNDEFINED, columnFixing=Component.UNDEFINED, columnHidingEnabled=Component.UNDEFINED, columnMinWidth=Component.UNDEFINED, columnResizingMode=Component.UNDEFINED, columns=Component.UNDEFINED, columnWidth=Component.UNDEFINED, customizeColumns=Component.UNDEFINED, customizeExportData=Component.UNDEFINED, dataSource=Component.UNDEFINED, dateSerializationFormat=Component.UNDEFINED, disabled=Component.UNDEFINED, editing=Component.UNDEFINED, elementAttr=Component.UNDEFINED, errorRowEnabled=Component.UNDEFINED, export=Component.UNDEFINED, filterBuilder=Component.UNDEFINED, filterBuilderPopup=Component.UNDEFINED, filterPanel=Component.UNDEFINED, filterRow=Component.UNDEFINED, filterSyncEnabled=Component.UNDEFINED, filterValue=Component.UNDEFINED, focusedColumnIndex=Component.UNDEFINED, focusedRowEnabled=Component.UNDEFINED, focusedRowIndex=Component.UNDEFINED, focusedRowKey=Component.UNDEFINED, focusStateEnabled=Component.UNDEFINED, grouping=Component.UNDEFINED, groupPanel=Component.UNDEFINED, headerFilter=Component.UNDEFINED, height=Component.UNDEFINED, highlightChanges=Component.UNDEFINED, hint=Component.UNDEFINED, hoverStateEnabled=Component.UNDEFINED, keyExpr=Component.UNDEFINED, loadPanel=Component.UNDEFINED, masterDetail=Component.UNDEFINED, noDataText=Component.UNDEFINED, onAdaptiveDetailRowPreparing=Component.UNDEFINED, onCellClick=Component.UNDEFINED, onCellHoverChanged=Component.UNDEFINED, onCellPrepared=Component.UNDEFINED, onContentReady=Component.UNDEFINED, onContextMenuPreparing=Component.UNDEFINED, onDataErrorOccurred=Component.UNDEFINED, onDisposing=Component.UNDEFINED, onEditingStart=Component.UNDEFINED, onEditorPrepared=Component.UNDEFINED, onEditorPreparing=Component.UNDEFINED, onExported=Component.UNDEFINED, onExporting=Component.UNDEFINED, onFileSaving=Component.UNDEFINED, onFocusedCellChanged=Component.UNDEFINED, onFocusedCellChanging=Component.UNDEFINED, onFocusedRowChanged=Component.UNDEFINED, onFocusedRowChanging=Component.UNDEFINED, onInitialized=Component.UNDEFINED, onInitNewRow=Component.UNDEFINED, onKeyDown=Component.UNDEFINED, onOptionChanged=Component.UNDEFINED, onRowClick=Component.UNDEFINED, onRowCollapsed=Component.UNDEFINED, onRowCollapsing=Component.UNDEFINED, onRowExpanded=Component.UNDEFINED, onRowExpanding=Component.UNDEFINED, onRowInserted=Component.UNDEFINED, onRowInserting=Component.UNDEFINED, onRowPrepared=Component.UNDEFINED, onRowRemoved=Component.UNDEFINED, onRowRemoving=Component.UNDEFINED, onRowUpdated=Component.UNDEFINED, onRowUpdating=Component.UNDEFINED, onRowValidating=Component.UNDEFINED, onSelectionChanged=Component.UNDEFINED, onToolbarPreparing=Component.UNDEFINED, pager=Component.UNDEFINED, paging=Component.UNDEFINED, remoteOperations=Component.UNDEFINED, renderAsync=Component.UNDEFINED, repaintChangesOnly=Component.UNDEFINED, rowAlternationEnabled=Component.UNDEFINED, rowTemplate=Component.UNDEFINED, rtlEnabled=Component.UNDEFINED, scrolling=Component.UNDEFINED, searchPanel=Component.UNDEFINED, selectedRowKeys=Component.UNDEFINED, selection=Component.UNDEFINED, selectionFilter=Component.UNDEFINED, showBorders=Component.UNDEFINED, showColumnHeaders=Component.UNDEFINED, showColumnLines=Component.UNDEFINED, showRowLines=Component.UNDEFINED, sortByGroupSummaryInfo=Component.UNDEFINED, sorting=Component.UNDEFINED, stateStoring=Component.UNDEFINED, summary=Component.UNDEFINED, tabIndex=Component.UNDEFINED, twoWayBindingEnabled=Component.UNDEFINED, visible=Component.UNDEFINED, width=Component.UNDEFINED, wordWrapEnabled=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'accessKey', 'activeStateEnabled', 'allowColumnReordering', 'allowColumnResizing', 'cacheEnabled', 'cellHintEnabled', 'columnAutoWidth', 'columnChooser', 'columnFixing', 'columnHidingEnabled', 'columnMinWidth', 'columnResizingMode', 'columns', 'columnWidth', 'dataSource', 'dateSerializationFormat', 'disabled', 'editing', 'elementAttr', 'errorRowEnabled', 'export', 'filterBuilder', 'filterBuilderPopup', 'filterPanel', 'filterRow', 'filterSyncEnabled', 'filterValue', 'focusedColumnIndex', 'focusedRowEnabled', 'focusedRowIndex', 'focusedRowKey', 'focusStateEnabled', 'grouping', 'groupPanel', 'headerFilter', 'height', 'highlightChanges', 'hint', 'hoverStateEnabled', 'keyExpr', 'loadPanel', 'masterDetail', 'noDataText', 'onCellClick', 'onRowClick', 'pager', 'paging', 'remoteOperations', 'renderAsync', 'repaintChangesOnly', 'rowAlternationEnabled', 'rowTemplate', 'rtlEnabled', 'scrolling', 'searchPanel', 'selectedRowKeys', 'selection', 'selectionFilter', 'showBorders', 'showColumnHeaders', 'showColumnLines', 'showRowLines', 'sortByGroupSummaryInfo', 'sorting', 'stateStoring', 'summary', 'tabIndex', 'twoWayBindingEnabled', 'visible', 'width', 'wordWrapEnabled']
         self._type = 'DataGrid'
         self._namespace = 'dash_devextreme'
         self._valid_wildcard_attributes =            []
         self.available_events = []
-        self.available_properties = ['id', 'dataSource', 'columns', 'columnAutoWidth', 'grouping', 'rowTemplate']
+        self.available_properties = ['id', 'accessKey', 'activeStateEnabled', 'allowColumnReordering', 'allowColumnResizing', 'cacheEnabled', 'cellHintEnabled', 'columnAutoWidth', 'columnChooser', 'columnFixing', 'columnHidingEnabled', 'columnMinWidth', 'columnResizingMode', 'columns', 'columnWidth', 'dataSource', 'dateSerializationFormat', 'disabled', 'editing', 'elementAttr', 'errorRowEnabled', 'export', 'filterBuilder', 'filterBuilderPopup', 'filterPanel', 'filterRow', 'filterSyncEnabled', 'filterValue', 'focusedColumnIndex', 'focusedRowEnabled', 'focusedRowIndex', 'focusedRowKey', 'focusStateEnabled', 'grouping', 'groupPanel', 'headerFilter', 'height', 'highlightChanges', 'hint', 'hoverStateEnabled', 'keyExpr', 'loadPanel', 'masterDetail', 'noDataText', 'onCellClick', 'onRowClick', 'pager', 'paging', 'remoteOperations', 'renderAsync', 'repaintChangesOnly', 'rowAlternationEnabled', 'rowTemplate', 'rtlEnabled', 'scrolling', 'searchPanel', 'selectedRowKeys', 'selection', 'selectionFilter', 'showBorders', 'showColumnHeaders', 'showColumnLines', 'showRowLines', 'sortByGroupSummaryInfo', 'sorting', 'stateStoring', 'summary', 'tabIndex', 'twoWayBindingEnabled', 'visible', 'width', 'wordWrapEnabled']
         self.available_wildcard_properties =            []
 
         _explicit_args = kwargs.pop('_explicit_args')
--- src/lib/components/DataGrid.react.js
+++ src/lib/components/DataGrid.react.js
@@ -53,7 +53,7 @@ export default class DataGrid extends Component {
     }
 
     render() {
-        const {id, dataSource, setProps, columns} = this.props;
+        const {columns, setProps} = this.props;
         let {rowTemplate} = this.props;
 
         this._processCellTemplate(columns);
@@ -63,9 +63,7 @@ export default class DataGrid extends Component {
         }
 
         return (
-            <DXDataGrid
-                id={id}
-                dataSource={dataSource}
+            <DXDataGrid {...this.props}
                 columns={columns}
                 rowTemplate={rowTemplate}
                 onCellClick={e => {
@@ -91,8 +89,6 @@ export default class DataGrid extends Component {
                         });
                     }
                 }}
-                columnAutoWidth={this.props.columnAutoWidth}
-                grouping={this.props.grouping}
             />
         );
     }
@@ -101,17 +97,226 @@ export default class DataGrid extends Component {
 DataGrid.propTypes = {
     id: PropTypes.string,
 
-    dataSource: PropTypes.object,
-    columns: PropTypes.arrayOf(PropTypes.shape),
+    accessKey: PropTypes.string,
+    activeStateEnabled: PropTypes.bool,
+    allowColumnReordering: PropTypes.bool,
+    allowColumnResizing: PropTypes.bool,
+    cacheEnabled: PropTypes.bool,
+    cellHintEnabled: PropTypes.bool,
     columnAutoWidth: PropTypes.bool,
+    columnChooser: PropTypes.object,
+    columnFixing: PropTypes.object,
+    columnHidingEnabled: PropTypes.bool,
+    columnMinWidth: PropTypes.number,
+    columnResizingMode: PropTypes.oneOf(['nextColumn', 'widget']),
+    columns: PropTypes.arrayOf(PropTypes.object),
+    columnWidth: PropTypes.number,
+    customizeColumns: PropTypes.func,
+    customizeExportData: PropTypes.func,
+    dataSource: PropTypes.oneOfType([PropTypes.object, PropTypes.arrayOf(PropTypes.object)]),
+    dateSerializationFormat: PropTypes.string,
+    disabled: PropTypes.bool,
+    editing: PropTypes.object,
+    elementAttr: PropTypes.object,
+    errorRowEnabled: PropTypes.bool,
+    export: PropTypes.object,
+    filterBuilder: PropTypes.object,
+    filterBuilderPopup: PropTypes.object,
+    filterPanel: PropTypes.object,
+    filterRow: PropTypes.object,
+    filterSyncEnabled: PropTypes.bool,
+    filterValue: PropTypes.object,
+    focusedColumnIndex: PropTypes.number,
+    focusedRowEnabled: PropTypes.bool,
+    focusedRowIndex: PropTypes.number,
+    focusedRowKey: PropTypes.any,
+    focusStateEnabled: PropTypes.bool,
     grouping: PropTypes.object,
-
+    groupPanel: PropTypes.object,
+    headerFilter: PropTypes.object,
+    height: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.func]),
+    highlightChanges: PropTypes.bool,
+    hint: PropTypes.string,
+    hoverStateEnabled: PropTypes.bool,
+    keyExpr: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
+    loadPanel: PropTypes.object,
+    masterDetail: PropTypes.object,
+    noDataText: PropTypes.string,
+    onAdaptiveDetailRowPreparing: PropTypes.func,
+    onCellClick: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+    onCellHoverChanged: PropTypes.func,
+    onCellPrepared: PropTypes.func,
+    onContentReady: PropTypes.func,
+    onContextMenuPreparing: PropTypes.func,
+    onDataErrorOccurred: PropTypes.func,
+    onDisposing: PropTypes.func,
+    onEditingStart: PropTypes.func,
+    onEditorPrepared: PropTypes.func,
+    onEditorPreparing: PropTypes.func,
+    onExported: PropTypes.func,
+    onExporting: PropTypes.func,
+    onFileSaving: PropTypes.func,
+    onFocusedCellChanged: PropTypes.func,
+    onFocusedCellChanging: PropTypes.func,
+    onFocusedRowChanged: PropTypes.func,
+    onFocusedRowChanging: PropTypes.func,
+    onInitialized: PropTypes.func,
+    onInitNewRow: PropTypes.func,
+    onKeyDown: PropTypes.func,
+    onOptionChanged: PropTypes.func,
+    onRowClick: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+    onRowCollapsed: PropTypes.func,
+    onRowCollapsing: PropTypes.func,
+    onRowExpanded: PropTypes.func,
+    onRowExpanding: PropTypes.func,
+    onRowInserted: PropTypes.func,
+    onRowInserting: PropTypes.func,
+    onRowPrepared: PropTypes.func,
+    onRowRemoved: PropTypes.func,
+    onRowRemoving: PropTypes.func,
+    onRowUpdated: PropTypes.func,
+    onRowUpdating: PropTypes.func,
+    onRowValidating: PropTypes.func,
+    onSelectionChanged: PropTypes.func,
+    onToolbarPreparing: PropTypes.func,
+    pager: PropTypes.object,
+    paging: PropTypes.object,
+    remoteOperations: PropTypes.oneOfType([PropTypes.bool, PropTypes.object]),
+    renderAsync: PropTypes.bool,
+    repaintChangesOnly: PropTypes.bool,
+    rowAlternationEnabled: PropTypes.bool,
     rowTemplate: PropTypes.string,
+    rtlEnabled: PropTypes.bool,
+    scrolling: PropTypes.object,
+    searchPanel: PropTypes.object,
+    selectedRowKeys: PropTypes.arrayOf(PropTypes.any),
+    selection: PropTypes.object,
+    selectionFilter: PropTypes.arrayOf(PropTypes.shape),
+    showBorders: PropTypes.bool,
+    showColumnHeaders: PropTypes.bool,
+    showColumnLines: PropTypes.bool,
+    showRowLines: PropTypes.bool,
+    sortByGroupSummaryInfo: PropTypes.arrayOf(PropTypes.object),
+    sorting: PropTypes.object,
+    stateStoring: PropTypes.object,
+    summary: PropTypes.object,
+    tabIndex: PropTypes.number,
+    twoWayBindingEnabled: PropTypes.bool,
+    visible: PropTypes.bool,
+    width: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.func]),
+    wordWrapEnabled: PropTypes.bool,
 
     // Dash supplied props
     setProps: PropTypes.func
 };
 
 DataGrid.defaultProps = {
-    columnAutoWidth: true
+    accessKey: null,
+    activeStateEnabled: false,
+    allowColumnReordering: false,
+    allowColumnResizing: false,
+    cacheEnabled: true,
+    cellHintEnabled: true,
+    columnAutoWidth: false,
+    // columnChooser
+    // columnFixing
+    columnHidingEnabled: false,
+    // columnMinWidth
+    columnResizingMode: 'nextColumn',
+    // columns
+    // columnWidth
+    // customizeColumns
+    // customizeExportData
+    dataSource: null,
+    // dateSerializationFormat
+    disabled: false,
+    // editing
+    elementAttr: {},
+    errorRowEnabled: true,
+    // export
+    filterBuilder: {},
+    filterBuilderPopup: {},
+    filterPanel: {},
+    // filterRow
+    // filterSyncEnabled
+    filterValue: null,
+    focusedColumnIndex: -1,
+    focusedRowEnabled: false,
+    focusedRowIndex: -1,
+    // focusedRowKey
+    focusStateEnabled: false,
+    // grouping
+    // groupPanel
+    // headerFilter
+    // height
+    highlightChanges: false,
+    // hint
+    hoverStateEnabled: false,
+    // keyExpr
+    // loadPanel
+    // masterDetail
+    noDataText: 'No data',
+    onAdaptiveDetailRowPreparing: null,
+    onCellClick: null,
+    onCellHoverChanged: null,
+    onCellPrepared: null,
+    onContentReady: null,
+    onContextMenuPreparing: null,
+    onDataErrorOccurred: null,
+    onDisposing: null,
+    onEditingStart: null,
+    onEditorPrepared: null,
+    onEditorPreparing: null,
+    onExported: null,
+    onExporting: null,
+    onFileSaving: null,
+    onFocusedCellChanged: null,
+    onFocusedCellChanging: null,
+    onFocusedRowChanged: null,
+    onFocusedRowChanging: null,
+    onInitialized: null,
+    onInitNewRow: null,
+    onKeyDown: null,
+    onOptionChanged: null,
+    onRowClick: null,
+    onRowCollapsed: null,
+    onRowCollapsing: null,
+    onRowExpanded: null,
+    onRowExpanding: null,
+    onRowInserted: null,
+    onRowInserting: null,
+    onRowPrepared: null,
+    onRowRemoved: null,
+    onRowRemoving: null,
+    onRowUpdated: null,
+    onRowUpdating: null,
+    onRowValidating: null,
+    onSelectionChanged: null,
+    onToolbarPreparing: null,
+    // pager
+    // paging
+    // remoteOperations
+    renderAsync: false,
+    repaintChangesOnly: false,
+    rowAlternationEnabled: false,
+    // rowTemplate
+    rtlEnabled: false,
+    // scrolling
+    // searchPanel
+    // selectedRowKeys
+    // selection
+    selectionFilter: [],
+    showBorders: false,
+    showColumnHeaders: true,
+    showColumnLines: true, // false (Material)
+    showRowLines: false, // true (iOS, Material)
+    // sortByGroupSummaryInfo
+    // sorting
+    // stateStoring
+    // summary
+    tabIndex: 0,
+    twoWayBindingEnabled: true,
+    visible: true,
+    // width
+    wordWrapEnabled: false,
 };
