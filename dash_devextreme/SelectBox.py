544e0a31a615c0cddfebceb86d2b5fcb8828a9eb
Sergey Pikhovkin <pikhovkin@gmail.com>
2019-02-03 00:23:35 +0300

    Update SelectBox

--- dash_devextreme/SelectBox.py
+++ dash_devextreme/SelectBox.py
@@ -9,18 +9,63 @@ class SelectBox(Component):
 
 Keyword arguments:
 - id (string; optional)
-- dataSource (dict; optional)
-- value (string | number; optional)
+- acceptCustomValue (boolean; optional)
+- accessKey (string; optional)
+- activeStateEnabled (boolean; optional)
+- dataSource (string | list; optional)
+- deferRendering (boolean; optional)
+- disabled (boolean; optional)
+- displayExpr (string; optional)
+- dropDownButtonTemplate (string; optional)
+- elementAttr (dict; optional)
+- fieldTemplate (string; optional)
+- focusStateEnabled (boolean; optional)
+- grouped (boolean; optional)
+- groupTemplate (string; optional)
+- height (number | string; optional)
+- hint (string; optional)
+- hoverStateEnabled (boolean; optional)
+- inputAttr (dict; optional)
+- isValid (boolean; optional)
+- dxItems (list; optional)
+- itemTemplate (string; optional)
+- maxLength (string | number; optional)
+- minSearchLength (number; optional)
+- name (string; optional)
+- noDataText (string; optional)
+- opened (boolean; optional)
+- openOnFieldClick (boolean; optional)
+- placeholder (string; optional)
+- readOnly (boolean; optional)
+- rtlEnabled (boolean; optional)
+- searchEnabled (boolean; optional)
+- searchExpr (string | list; optional)
+- searchMode (a value equal to: 'contains', 'startswith'; optional)
+- searchTimeout (number; optional)
+- showClearButton (boolean; optional)
+- showDataBeforeSearch (boolean; optional)
+- showDropDownButton (boolean; optional)
+- showSelectionControls (boolean; optional)
+- spellcheck (boolean; optional)
+- stylingMode (a value equal to: 'outlined', 'underlined', 'filled'; optional)
+- tabIndex (number; optional)
+- validationError (dict; optional)
+- validationMessageMode (a value equal to: 'always', 'auto'; optional)
+- value (boolean | number | string | dict | list; optional)
+- valueChangeEvent (string; optional)
+- valueExpr (string; optional)
+- visible (boolean; optional)
+- width (number | string; optional)
 
 Available events: 'change'"""
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, dataSource=Component.UNDEFINED, value=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'dataSource', 'value']
+    def __init__(self, id=Component.UNDEFINED, acceptCustomValue=Component.UNDEFINED, accessKey=Component.UNDEFINED, activeStateEnabled=Component.UNDEFINED, dataSource=Component.UNDEFINED, deferRendering=Component.UNDEFINED, disabled=Component.UNDEFINED, displayExpr=Component.UNDEFINED, dropDownButtonTemplate=Component.UNDEFINED, elementAttr=Component.UNDEFINED, fieldTemplate=Component.UNDEFINED, focusStateEnabled=Component.UNDEFINED, grouped=Component.UNDEFINED, groupTemplate=Component.UNDEFINED, height=Component.UNDEFINED, hint=Component.UNDEFINED, hoverStateEnabled=Component.UNDEFINED, inputAttr=Component.UNDEFINED, isValid=Component.UNDEFINED, dxItems=Component.UNDEFINED, itemTemplate=Component.UNDEFINED, maxLength=Component.UNDEFINED, minSearchLength=Component.UNDEFINED, name=Component.UNDEFINED, noDataText=Component.UNDEFINED, onChange=Component.UNDEFINED, onClosed=Component.UNDEFINED, onContentReady=Component.UNDEFINED, onCopy=Component.UNDEFINED, onCustomItemCreating=Component.UNDEFINED, onCut=Component.UNDEFINED, onDisposing=Component.UNDEFINED, onEnterKey=Component.UNDEFINED, onFocusIn=Component.UNDEFINED, onFocusOut=Component.UNDEFINED, onInitialized=Component.UNDEFINED, onInput=Component.UNDEFINED, onItemClick=Component.UNDEFINED, onKeyDown=Component.UNDEFINED, onKeyPress=Component.UNDEFINED, onKeyUp=Component.UNDEFINED, onOpened=Component.UNDEFINED, onOptionChanged=Component.UNDEFINED, onPaste=Component.UNDEFINED, onSelectionChanged=Component.UNDEFINED, onValueChanged=Component.UNDEFINED, opened=Component.UNDEFINED, openOnFieldClick=Component.UNDEFINED, placeholder=Component.UNDEFINED, readOnly=Component.UNDEFINED, rtlEnabled=Component.UNDEFINED, searchEnabled=Component.UNDEFINED, searchExpr=Component.UNDEFINED, searchMode=Component.UNDEFINED, searchTimeout=Component.UNDEFINED, showClearButton=Component.UNDEFINED, showDataBeforeSearch=Component.UNDEFINED, showDropDownButton=Component.UNDEFINED, showSelectionControls=Component.UNDEFINED, spellcheck=Component.UNDEFINED, stylingMode=Component.UNDEFINED, tabIndex=Component.UNDEFINED, validationError=Component.UNDEFINED, validationMessageMode=Component.UNDEFINED, value=Component.UNDEFINED, valueChangeEvent=Component.UNDEFINED, valueExpr=Component.UNDEFINED, visible=Component.UNDEFINED, width=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'acceptCustomValue', 'accessKey', 'activeStateEnabled', 'dataSource', 'deferRendering', 'disabled', 'displayExpr', 'dropDownButtonTemplate', 'elementAttr', 'fieldTemplate', 'focusStateEnabled', 'grouped', 'groupTemplate', 'height', 'hint', 'hoverStateEnabled', 'inputAttr', 'isValid', 'dxItems', 'itemTemplate', 'maxLength', 'minSearchLength', 'name', 'noDataText', 'opened', 'openOnFieldClick', 'placeholder', 'readOnly', 'rtlEnabled', 'searchEnabled', 'searchExpr', 'searchMode', 'searchTimeout', 'showClearButton', 'showDataBeforeSearch', 'showDropDownButton', 'showSelectionControls', 'spellcheck', 'stylingMode', 'tabIndex', 'validationError', 'validationMessageMode', 'value', 'valueChangeEvent', 'valueExpr', 'visible', 'width']
         self._type = 'SelectBox'
         self._namespace = 'dash_devextreme'
         self._valid_wildcard_attributes =            []
         self.available_events = ['change']
-        self.available_properties = ['id', 'dataSource', 'value']
+        self.available_properties = ['id', 'acceptCustomValue', 'accessKey', 'activeStateEnabled', 'dataSource', 'deferRendering', 'disabled', 'displayExpr', 'dropDownButtonTemplate', 'elementAttr', 'fieldTemplate', 'focusStateEnabled', 'grouped', 'groupTemplate', 'height', 'hint', 'hoverStateEnabled', 'inputAttr', 'isValid', 'dxItems', 'itemTemplate', 'maxLength', 'minSearchLength', 'name', 'noDataText', 'opened', 'openOnFieldClick', 'placeholder', 'readOnly', 'rtlEnabled', 'searchEnabled', 'searchExpr', 'searchMode', 'searchTimeout', 'showClearButton', 'showDataBeforeSearch', 'showDropDownButton', 'showSelectionControls', 'spellcheck', 'stylingMode', 'tabIndex', 'validationError', 'validationMessageMode', 'value', 'valueChangeEvent', 'valueExpr', 'visible', 'width']
         self.available_wildcard_properties =            []
 
         _explicit_args = kwargs.pop('_explicit_args')
--- src/lib/components/SelectBox.react.js
+++ src/lib/components/SelectBox.react.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import R from 'ramda';
+import * as R from 'ramda';
 
 import {SelectBox as DXSelectBox} from 'devextreme-react';
 
@@ -10,33 +10,44 @@ export default class SelectBox extends Component {
         super(props);
 
         this.state = {
-            value: props.value
+            value: props.value,
+            opened: props.opened
         };
 
         this.onValueChanged = this.onValueChanged.bind(this);
+        this.onOpened = this.onOpened.bind(this);
+        this.onFocusOut = this.onFocusOut.bind(this);
     }
 
     onValueChanged(e) {
         const {setProps, fireEvent} = this.props;
         if (setProps) {
             setProps({
-                value_changed: {value: e.value, previousValue: e.previousValue}
+                value: {value: e.value, previousValue: e.previousValue}
             });
         }
-        this.setState({value: e.value});
+        this.setState({value: e.value, opened: false});
         if (fireEvent) fireEvent({event: 'change'});
     }
 
-    render() {
-        const {id, dataSource} = this.props;
+    onOpened(e) {
+        this.setState({opened: true});
+    }
 
+    onFocusOut(e) {
+        this.setState({opened: false});
+    }
+
+    render() {
         return (
             <DXSelectBox
-                id={id}
-                dataSource={dataSource}
+                items={this.props.dxItems}
                 value={this.state.value}
-                onValueChanged={this.onValueChanged}
-                {...R.omit(['value', 'setProps', 'fireEvent'], this.props)}
+                opened={this.state.opened}
+                onValueChanged={this.props.onValueChanged || this.onValueChanged}
+                onOpened={this.props.onOpened || this.onOpened}
+                onFocusOut={this.props.onFocusOut || this.onFocusOut}
+                {...R.omit(['opened', 'value', 'setProps', 'fireEvent', 'onOpened', 'onFocusOut', 'onValueChanged', 'dxItems'], this.props)}
             />
         );
     }
@@ -44,18 +55,154 @@ export default class SelectBox extends Component {
 
 SelectBox.propTypes = {
     id: PropTypes.string,
-    dataSource: PropTypes.object,
-    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+
+    acceptCustomValue: PropTypes.bool,
+    accessKey: PropTypes.string,
+    activeStateEnabled: PropTypes.bool,
+    dataSource: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.any)]),
+    deferRendering: PropTypes.bool,
+    disabled: PropTypes.bool,
+    displayExpr: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+    dropDownButtonTemplate: PropTypes.string,
+    elementAttr: PropTypes.object,
+    fieldTemplate: PropTypes.string,
+    focusStateEnabled: PropTypes.bool,
+    grouped: PropTypes.bool,
+    groupTemplate: PropTypes.string,
+    height: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.func]),
+    hint: PropTypes.string,
+    hoverStateEnabled: PropTypes.bool,
+    inputAttr: PropTypes.object,
+    isValid: PropTypes.bool,
+    dxItems: PropTypes.arrayOf(PropTypes.any),
+    itemTemplate: PropTypes.string,
+    maxLength: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    minSearchLength: PropTypes.number,
+    name: PropTypes.string,
+    noDataText: PropTypes.string,
+    onChange: PropTypes.func,
+    onClosed: PropTypes.func,
+    onContentReady: PropTypes.func,
+    onCopy: PropTypes.func,
+    onCustomItemCreating: PropTypes.func,
+    onCut: PropTypes.func,
+    onDisposing: PropTypes.func,
+    onEnterKey: PropTypes.func,
+    onFocusIn: PropTypes.func,
+    onFocusOut: PropTypes.func,
+    onInitialized: PropTypes.func,
+    onInput: PropTypes.func,
+    onItemClick: PropTypes.func,
+    onKeyDown: PropTypes.func,
+    onKeyPress: PropTypes.func,
+    onKeyUp: PropTypes.func,
+    onOpened: PropTypes.func,
+    onOptionChanged: PropTypes.func,
+    onPaste: PropTypes.func,
+    onSelectionChanged: PropTypes.func,
+    onValueChanged: PropTypes.func,
+    opened: PropTypes.bool,
+    openOnFieldClick: PropTypes.bool,
+    placeholder: PropTypes.string,
+    readOnly: PropTypes.bool,
+    rtlEnabled: PropTypes.bool,
+    searchEnabled: PropTypes.bool,
+    searchExpr: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
+    searchMode: PropTypes.oneOf(['contains', 'startswith']),
+    searchTimeout: PropTypes.number,
+    showClearButton: PropTypes.bool,
+    showDataBeforeSearch: PropTypes.bool,
+    showDropDownButton: PropTypes.bool,
+    showSelectionControls: PropTypes.bool,
+    spellcheck: PropTypes.bool,
+    stylingMode: PropTypes.oneOf(['outlined', 'underlined', 'filled']),
+    tabIndex: PropTypes.number,
+    validationError: PropTypes.object,
+    validationMessageMode: PropTypes.oneOf(['always', 'auto']),
+    value: PropTypes.any,
+    valueChangeEvent: PropTypes.string,
+    valueExpr: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+    visible: PropTypes.bool,
+    width: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.func]),
 
     /**
      * Dash-assigned callback that gets fired when the checkbox item gets selected.
      */
     fireEvent: PropTypes.func,
-
     /**
      * Dash-assigned callback that gets fired when the value changes.
      */
     setProps: PropTypes.func,
-
     dashEvents: PropTypes.oneOf(['change']),
+};
+
+SelectBox.defaultProps = {
+    acceptCustomValue: false,
+    accessKey: null,
+    activeStateEnabled: true,
+    dataSource: null,
+    deferRendering: true,
+    disabled: false,
+    // displayExpr: undefined,
+    dropDownButtonTemplate: 'dropDownButton',
+    elementAttr: {},
+    fieldTemplate: null,
+    focusStateEnabled: true,
+    grouped: false,
+    groupTemplate: 'group',
+    // height: undefined,
+    // hint: undefined,
+    hoverStateEnabled: true,
+    inputAttr: {},
+    isValid: true,
+    dxItems: null,
+    itemTemplate: 'item',
+    maxLength: null,
+    minSearchLength: 0,
+    name: '',
+    noDataText: 'No data to display',
+    onChange: null,
+    onClosed: null,
+    onContentReady: null,
+    onCopy: null,
+    // onCustomItemCreating: function(e) { if(!e.customItem) { e.customItem = e.text; } },
+    onCut: null,
+    onDisposing: null,
+    onEnterKey: null,
+    onFocusIn: null,
+    onFocusOut: null,
+    onInitialized: null,
+    onInput: null,
+    onItemClick: null,
+    onKeyDown: null,
+    onKeyPress: null,
+    onKeyUp: null,
+    onOpened: null,
+    onOptionChanged: null,
+    onPaste: null,
+    onSelectionChanged: null,
+    onValueChanged: null,
+    opened: false,
+    openOnFieldClick: true,
+    placeholder: 'Select',
+    readOnly: false,
+    rtlEnabled: false,
+    searchEnabled: false,
+    searchExpr: null,
+    searchMode: 'contains',
+    searchTimeout: 500,
+    showClearButton: false,
+    showDataBeforeSearch: false,
+    showDropDownButton: true,
+    showSelectionControls: false,
+    spellcheck: false,
+    stylingMode: 'outlined',
+    tabIndex: 0,
+    // validationError: undefined,
+    validationMessageMode: 'auto',
+    value: null,
+    valueChangeEvent: 'change',
+    valueExpr: 'this',
+    visible: true,
+    // width: undefined
 };
